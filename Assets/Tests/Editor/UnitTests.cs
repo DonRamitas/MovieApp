a22782298c6687fbc7a1bbcb2c1d858058e61d9d
Nego <ztylspook@gmail.com>
2025-02-05 04:27:59 -0300

    csd

--- Assets/Tests/Editor/UnitTests.cs
+++ Assets/Tests/Editor/UnitTests.cs
@@ -8,7 +8,7 @@ public class APIHelperTests
     [Test]
     public async Task SearchMovie_ValidQuery_ReturnsResults()
     {
-        // Arrange
+        // Arrang
         PlayerPrefs.SetString("apiKey", "666d00beb8a3dc5d87ddf5259ab016b0"); // Configura la API Key
         string query = "Inception";
 
